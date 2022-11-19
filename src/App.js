d72b1bbc607eb75a6d72b100b56104f2a0ac9552
sunitadhikari <sunitaadhikari2001@gmail.com>
2022-11-19 15:18:52 +0545

    message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Component/Header'
+import "./App.css"
+
+const App = () => {
+  return (
+   <>
+   <Header/>
+   </>
+  )
+}
+
+export default App
---
+++ src/Component/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+   <>
+     <header className="header" id="home">
+        <nav className='navbar container'>
+            <a href="index.html" className="navbar-brard">
+            <i className="fa-sharp fa-solid fa-headphones"></i>
+            </a>
+        </nav>
+     </header>
+   </>
+  )
+}
+
+export default Header
